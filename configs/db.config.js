3db73d4a816bc4bb5ab98aa057a6b8644c41573f
sakshi <123829259+Ranasakshi12345@users.noreply.github.com>
2023-08-02 17:26:50 +0530

    Update db.config.js

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
     DB_NAME : "idea_app_demo",
-    DB_URL : "mongodb+srv://sakshi146:sakshi14@cluster0.as9aest.mongodb.net"
+    DB_URL : "mongodb+srv://sakshi146:password@cluster0.as9aest.mongodb.net"
 }
